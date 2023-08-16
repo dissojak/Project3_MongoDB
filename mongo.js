f808014cc9f0802dfd26e1db18f93956a26c2036
Adem Ben Amor <dissojak@gmail.com>
2023-08-16 22:41:27 +0100

    closing connection - error fix -

--- mongo.js
+++ mongo.js
@@ -2,6 +2,8 @@ const MongoClient = require("mongodb").MongoClient;
 const url =
   "mongodb+srv://dissojak:stoondissojakb2a@stoon.r8tcyqv.mongodb.net/Products?retryWrites=true&w=majority";
 
+// There is a problem with the Closing function , so i just commented it out !
+
 exports.createProduct = async (req, res, next) => {
   const { name, price } = req.body;
   const newProduct = {
@@ -13,22 +15,23 @@ exports.createProduct = async (req, res, next) => {
     await client.connect();
     const db = client.db();
     const result = db.collection("products").insertOne(newProduct);
-  
   } catch (e) {
     return res.json({ message: "Could not store data !" });
   }
+  // client.close();
   res.json(newProduct);
 };
 
 exports.getProduct = async (req, res, next) => {
   const client = new MongoClient(url);
   let products;
-try{
-await client.connect();
-const db = client.db();
-products = await db.collection("products").find().toArray();
-}catch (e) {
- return res.json({ message: "Could not retrieve product" });
-};
-res.json({products});
+  try {
+    await client.connect();
+    const db = client.db();
+    products = await db.collection("products").find().toArray();
+  } catch (e) {
+    return res.json({ message: "Could not retrieve product" });
+  }
+  // client.close();
+  res.json({ products });
 };
